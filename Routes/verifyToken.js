12f0cc691974c5ef2e91313c8898a8f5a773a2eb
benkwon <kwon5891@gmail.com>
2022-02-14 01:12:31 +0900

    fix: verifyToken함수에서 에러 발생시 에러 미들웨어로 이동안하는 현상 해결

--- Routes/verifyToken.js
+++ Routes/verifyToken.js
@@ -1,29 +1,30 @@
 const jwt = require('jsonwebtoken');
+const CustomError = require('../errors/CustomError');
 
 const verifyToken = (req, res, next) => {
-	console.log(req.body);
 	if (req.headers["x-access-token"]) {
 		const accessToken = req.headers["x-access-token"].split(" ")[1];
 		jwt.verify(accessToken, process.env.JWT_SEC, (err, user) => {
-			if (err) return next(new Error("verifyToken",403,"Token is not valid"));
+			if (err) {
+				return next(new CustomError("verifyToken",403,"Token is not valid"));
+			}
 			else {
 				req.user = user;
-                console.log(user);
 				next();
 			}
 		});
 	} else {
-		return next(new Error("verifyToken",401,"You are not Authenticated"));
+		return next(new CustomError("verifyToken",401,"You are not Authenticated"));
 	}
 };
 
 const verifyTokenAndAuthorization = (req, res, next) => {
-	verifyToken(req, res, () => {
-		if (req.user.id === req.params.userId || req.user.id == req.body.userId) {
+	verifyToken(req, res, (err) => {
+		if(err) return next(err);
+		if (req.user.id === req.params.userId || req.user.id == req.body.userId || req.query.userId == req.user.id) {
 			next();
 		} else {
-			// const error 
-			return next(new Error("verifyToken",403,"You are not allowed to do that"));
+			return next(new CustomError("verifyToken",403,"You are not allowed to do that"));
 		}
 	});
 };
