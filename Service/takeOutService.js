10d20632c47825f28e54d6cdb618e9f3a074f35a
benkwon <kwon5891@gmail.com>
2022-02-24 19:37:46 +0900

    fix: take out expired at

--- Service/takeOutService.js
+++ Service/takeOutService.js
@@ -1,12 +1,15 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const dayjs = require('dayjs');
+const priamTimezoneDiff =9;
 exports.addTakeOutRequest = async function(userId, capacity){
+    let expired_at = new Date();
+    expired_at.setDate(expired_at.getDate + priamTimezoneDiff + 7); // expired at 기간 ~7일 
     await prisma.takeout_queue.create({
         data:{
             userId,
             capacity,
-            expired_at: new Date()
+            expired_at
         }
     })
     return {
