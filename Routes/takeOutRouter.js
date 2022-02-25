12d2736c90ac78eb5224ce9ec2517a14bebe1fcc
benkwon <kwon5891@gmail.com>
2022-02-25 21:40:17 +0900

    feat: takeout (getTakeOutRequestDownladLinks, getTakeOutRequest) api 추가

--- Routes/takeOutRouter.js
+++ Routes/takeOutRouter.js
@@ -3,8 +3,12 @@ const { verifyToken, verifyTokenAndAuthorization } = require('./verifyToken');
 const takeOutService = require('../Service/takeOutService.js')
 const CustomError = require('../errors/CustomError');
 
-// take out queue 에 작업 추가
-router.post('', async (req, res, next) => {
+/**
+ * addTakeOutRequest
+ * EndPoint: [POST] /takeout
+ * Description: 유저의 테이크 아웃 요청을 큐에 등록합니다.
+ */
+router.post('',verifyTokenAndAuthorization, async (req, res, next) => {
     try {
         const { userId, capacity } = req.body;
         const {success, message} = await takeOutService.addTakeOutRequest(userId, capacity)
@@ -16,8 +20,46 @@ router.post('', async (req, res, next) => {
         console.error(err);
         next(new CustomError("TAKEOUT", 500, "Takeout 큐에 작업을 추가하지 못했습니다"));
     }
-
 })
 
+/**
+ * getTakeOutRequest
+ * EndPoint: [GET] /takeout?userId={userId}
+ * Description: 특정 유저의 테이크 아웃 요청 리스트를 반환합니다.
+ */
+router.get('', verifyTokenAndAuthorization, async (req,res)=>{
+    const { userId }  = req.query;
+    try{
+        const takeoutList = await takeOutService.getTakeOutRequest(userId);
+        console.log(takeoutList[0].created_at.toString())
+        res.status(200).json({
+            success:true,
+            takeoutList
+        })
+    }catch (err){
+        console.error(err);
+        next(err);
+    }
+})
+
+/**
+ * getTakeOutRequestDownladLinks
+ * EndPoint: [GET] /takeout/{takeoutId}?userId={userId}
+ * Description:  처리된 테이크 아웃의 다운로드 링크를 반환합니다
+ */
+ router.get('/:takeoutId', verifyTokenAndAuthorization, async (req,res,next)=>{
+    try{
+        const { takeoutId }  = req.params;
+        const { userId } = req.query;
+        const takeoutDownloadLinkList = await takeOutService.getTakeOutRequestDownladLinks(parseInt(takeoutId),parseInt(userId));
+        console.log(takeoutDownloadLinkList)
+        res.status(200).json({
+            success:true,
+            takeoutDownloadLinkList
+        })
+    }catch (err){
+        next(err);
+    }
+})
 
 module.exports = router;
--- Service/takeOutService.js
+++ Service/takeOutService.js
@@ -2,6 +2,8 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const dayjs = require('dayjs');
 const priamTimezoneDiff = 9;
+const CustomError = require('../errors/CustomError');
+
 exports.addTakeOutRequest = async function(userId, capacity){
     await prisma.takeout_queue.create({
         data:{
@@ -14,3 +16,55 @@ exports.addTakeOutRequest = async function(userId, capacity){
         message: "takout 요청이 큐에 추가되었습니다"
     }
 }
+
+exports.getTakeOutRequest = async function(userId){
+    try{
+        const takeoutList = await prisma.takeout_queue.findMany({
+            where:{
+                userId
+            },select:{
+                id: true,
+                capacity:true,
+                finish: true,
+                expired_at: true,
+                created_at: true,
+            }
+        });
+ 
+        return takeoutList;
+       
+    }catch(err){
+        throw new CustomError("TAKEOUT", 500, "유저의 테이크 아웃요청들을 가져오지 못했습니다");
+    }
+}
+
+
+exports.getTakeOutRequestDownladLinks = async function(takeoutId, userId){
+    try{
+        console.log("takoutid",takeoutId,"userId",userId)
+        const results = await prisma.takeout_result_path.findMany({
+            where:{
+                takeoutId
+            },
+            select:{
+                path:true,
+                size:true,
+            }
+        })
+        const takeoutDownloadLinkList = []
+        results.forEach(r => {
+            let elem = {
+                index : parseInt(r.path.split('-')[3].split('.zip')[0]),
+                link: process.env.WEB_API_URL + `/takeout/download/${userId}/${r.path}`,
+                size: r.size
+            }
+            takeoutDownloadLinkList.push(elem);
+        }) 
+        return takeoutDownloadLinkList;
+       
+    }catch(err){
+        console.log(err);
+        throw new CustomError("TAKEOUT", 500, "해당 테이크 아웃 zip파일의 다운로드 링크들을 가져오지 못했습니다");
+    }
+}
+
